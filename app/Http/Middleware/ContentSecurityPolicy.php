1b23741876c67cf4a81179dbdda6df09f9864091
unknown <saintraphael2@gmailcom>
2024-12-07 18:39:13 +0000

    saveof071220241839

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+{
+    $response = $next($request);
+
+    // Set the Content-Security-Policy header
+    $response->headers->set('Content-Security-Policy', 'upgrade-insecure-requests;');
+
+    return $response;
+}
+
+}
