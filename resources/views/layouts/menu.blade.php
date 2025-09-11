784c6aa4b3e7b46fd65d7aa7e7660ea3ff4485e7
unknown <saintraphael2@gmailcom>
2025-09-11 23:26:00 +0000

    saveof11092025

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,28 +6,28 @@
     </a>
 </li>
 
-<li class="nav-item">
+<li class="nav-item" style="display:{{ visible ( 'produitBoutiques',$listeUrl ) }}">
     <a href="{{ route('produitBoutiques.index') }}" class="nav-link {{ Request::is('produitBoutiques*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Produit Boutiques</p>
     </a>
 </li>
 
-<li class="nav-item">
+<li class="nav-item" style="display:{{ visible ( 'listeProduit',$listeUrl ) }}">
     <a href="{{ route('listeProduit') }}" class="nav-link {{ Request::is('listeProduit*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Liste des Produits</p>
     </a>
 </li>
 
-<li class="nav-item">
+<li class="nav-item" style="display:{{ visible ( 'boutiques',$listeUrl ) }}">
     <a href="{{ route('boutiques.index') }}" class="nav-link {{ Request::is('boutiques*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Caisse</p>
     </a>
 </li>
 
-<li class="nav-item">
+<li class="nav-item" style="display:{{ visible ( 'stocks',$listeUrl ) }}">
     <a href="{{ route('stocks.index') }}" class="nav-link {{ Request::is('stocks*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Stocks</p>
@@ -38,7 +38,7 @@
 
 
 
-<li class="nav-item">
+<li class="nav-item" style="display:{{ visible ( 'magasin',$listeUrl ) }}">
     <a href="#" class="nav-link">
         <i class="nav-icon fas fa-chart-pie"></i>
         <p>
@@ -47,22 +47,31 @@
         </p>
     </a>
     <ul class="nav nav-treeview">
-    <li class="nav-item">
-    <a href="{{ route('sortieMagasin.index') }}" class="nav-link {{ Request::is('sortieMagasin*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Sortie d'articles</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('livraisons.index') }}" class="nav-link {{ Request::is('livraison*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Etat des livraisons</p>
-    </a>
-</li>
-
-      
-      
-</li>
+        <li class="nav-item" style="display:{{ visible ( 'sortieMagasin',$listeUrl ) }}">
+        <a href="{{ route('sortieMagasin.index') }}" class="nav-link {{ Request::is('sortieMagasin*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Sortie d'articles</p>
+        </a>
+        </li>
+        <li class="nav-item" style="display:{{ visible ( 'livraisons',$listeUrl ) }}">
+            <a href="{{ route('livraisons.index') }}" class="nav-link {{ Request::is('livraison*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-home"></i>
+                <p>Etat des livraisons</p>
+            </a>
+        </li>
     </ul>
 </li>
 
+<li class="nav-item" style="display:{{ visible ( 'users',$listeUrl ) }}">
+    <a href="{{ route('users.index') }}" class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-users"></i>
+        <p>Utilisateurs</p>
+    </a>
+</li>
+
+<li class="nav-item" style="display:{{ visible ( 'liens',$listeUrl ) }}">
+    <a href="{{ route('liens.index') }}" class="nav-link {{ Request::is('liens*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-link"></i>
+        <p>Liens</p>
+    </a>
+</li>
