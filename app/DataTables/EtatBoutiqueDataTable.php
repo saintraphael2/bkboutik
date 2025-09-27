8b4523511a26c4c20133b9056b044ec606ea173f
unknown <saintraphael2@gmailcom>
2025-09-27 19:26:25 +0000

    saveof2709

--- app/DataTables/EtatBoutiqueDataTable.php
+++ app/DataTables/EtatBoutiqueDataTable.php
@@ -102,7 +102,7 @@ class EtatBoutiqueDataTable extends DataTable
                 'buttons'   => [
                     // Enable Buttons as per your need
 //                    ['extend' => 'create', 'className' => 'btn btn-default btn-sm no-corner',],
-//                    ['extend' => 'export', 'className' => 'btn btn-default btn-sm no-corner',],
+                    ['extend' => 'export', 'className' => 'btn btn-default btn-sm no-corner',],
 //                    ['extend' => 'print', 'className' => 'btn btn-default btn-sm no-corner',],
 //                    ['extend' => 'reset', 'className' => 'btn btn-default btn-sm no-corner',],
 //                    ['extend' => 'reload', 'className' => 'btn btn-default btn-sm no-corner',],
